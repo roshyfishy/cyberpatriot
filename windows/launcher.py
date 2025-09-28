50ba34f2dba0fdf422083d4179fd3688203c7266
Roshy <mr_unknowny@outlook.com>
2025-09-28 15:00:14 -0400

    created the python file with burner plate code. Planning to use PySimpleGUI for GUI.

---
+++ windows/launcher.py
@@ -0,0 +1,15 @@
+import subprocess
+import sys
+import os
+
+dir = os.path.dirname(os.path.abspath(__file__))
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+def path(*paths):
+    return os.path.abspath(os.path.join(dir, *paths))
+
+with open(path("dependencies.txt")) as f:
+    for line in f:
+        install(line.strip())
+
