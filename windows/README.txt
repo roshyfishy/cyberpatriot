42685358341475dd141097579ab021a7deb92b01
Roshy <mr_unknowny@outlook.com>
2025-10-20 20:00:51 -0400

    Made the code crash if readme file cannot be read lol. Also deleting/adding or changing rights of user accounts currently show up as warnings cause I don't want to implement them yet. Everything I added should work though: > makeAllSecurePasswords() > userPasswordsExpire() > makeUsersChangePasswordNextLogon() > disableDefaultAccounts() Logging rn is kinda inconsistent, will fix eventually I hope. Also I created a README.txt and todo.txt (cool ig)

---
+++ windows/README.txt
@@ -0,0 +1,5 @@
+Anyone is free to use/replicate/distrubute the code with changes!!! (just dont sue me)
+Commits are appreciated! Please try to keep the code formatting consistent
+It is not recommended to run this on your personal computer unless you understand what the program does.
+To add on to the above, please either run it in a virtual computer (ex VMWare or Windows Sandbox) or during the competition! (but understand the risks lol)
+If you are going to use it during the competition, please don't blindly click "yes" to everything. Take time to understand what is going on behind the scenes so as not to brick your image.
--- windows/components/userAccounts.py
+++ windows/components/userAccounts.py
@@ -5,18 +5,38 @@ from logger import logger
 log = logger()
 log.debug("Logger initialized.")
 readme = os.path.abspath(os.path.join(os.path.dirname(__file__), "../components/readme.txt")) # will be changed later to actually retrieve readme from website
+you = ""
 admins = []
 users = []
-defaults = ["Administrator", "Guest", "DefaultAccount", "WDAGUtilityAccount", "WsiAccount"]
+known_defaults = ["Administrator", "Guest", "DefaultAccount", "WDAGUtilityAccount", "WsiAccount", "INTERACTIVE", "Authenticated Users"]
+defaults = []
+
 try:
     with open(readme, "r") as f:
         lines = [line.strip() for line in f.readlines()]
     if not lines:
         log.warning("readme file is empty.")
-    admins = lines[lines.index("Authorized Administrators:")+1:lines.index("Authorized Users:")-1][::2]
-    users = lines[lines.index("Authorized Users:")+1:lines.index("Competition Guidelines")-1]
+    try:
+        admins = lines[lines.index("Authorized Administrators:")+1:lines.index("Authorized Users:")-1][::2]
+        you = admins.pop(0).replace(" (you)", "").strip()
+    except ValueError:
+        raise Exception(ValueError("Missing Authorized Administrators section in readme file"))
+    try:
+        users = lines[lines.index("Authorized Users:")+1:lines.index("Competition Guidelines")-1]
+    except ValueError:
+        raise Exception(ValueError("Missing Authorized Users section in readme file"))
 except Exception as e:
-    log.error(f"Could not read readme file: {e}")
+    log.critical(f"Could not read readme file: {e}")
+    raise Exception(f"Could not read readme file: {e}")
+
+defaults.extend([u for u in users if u in known_defaults])
+users = [u for u in users if u not in known_defaults]
+
+defaults.extend([a for a in admins if a in known_defaults])
+admins = [a for a in admins if a not in known_defaults]
+
+
+
 
 def safe_call(func, label):
     try:
@@ -27,7 +47,8 @@ def safe_call(func, label):
 
 def verifyUsers():
     command = [
-        "powershell",
+        "powershell.exe",
+        "-NoProfile",
         "-Command",
         "Get-LocalUser | Select-Object -ExpandProperty Name"
     ]
@@ -35,13 +56,14 @@ def verifyUsers():
     for account in result:
         if account in users or account in admins or account in defaults:
             continue
-        log.warning(f"Unauthorized account found: {account}")
-        # todo: create pop up that asks for confirmation to delete account before deleting
+        log.warning(f"verifyUsers - Unauthorized account found: {account}")
+        # todo: create pop up that asks for confirmation to delete account before deleting and creates missing accounts
     return ""
 
 def verifyAccountType():
     command = [
-        "powershell",
+        "powershell.exe",
+        "-NoProfile",
         "-Command",
         "Get-LocalGroupMember -Group 'Administrators' | Select-Object -ExpandProperty Name"
     ]
@@ -49,27 +71,98 @@ def verifyAccountType():
     result = [account.split("\\")[-1] for account in result]
     unique_admins = list(set(result) ^ set(admins))
     for admin in unique_admins:
-        if admin in defaults:
+        if admin in defaults or admin == you:   
             continue
         if admin in result:
-            log.warning(f"Unauthorized administrator account found: {admin}")
+            log.warning(f"verifyAccountType - Unauthorized administrator account found: {admin}")
             continue
         if admin in admins:
-            log.warning(f"Administrator account missing or without admin rights: {admin}")
-    # todo: create pop up that asks for confirmation before removing admin rights
+            log.warning(f"verifyAccountType - Administrator account missing or without admin rights: {admin}")
+    # todo: create pop up that asks for confirmation before removing/adding admin rights or deleting the user
+    command = [
+        "powershell.exe",
+        "-NoProfile",
+        "-Command",
+        "Get-LocalGroupMember -Group 'Users' | Select-Object -ExpandProperty Name"
+    ]
+    result = subprocess.run(command, capture_output=True, text=True).stdout.splitlines()
+    result = [account.split("\\")[-1] for account in result]
+    unique_users = list(set(result) ^ set(users))
+    for user in unique_users:
+        if user in defaults or user in admins or user == you:
+            continue
+        if user in result:
+            log.warning(f"verifyAccountType - Unauthorized user account found: {user}")
+            continue
+        if user in users:
+            log.warning(f"verifyAccountType - User account missing or with admin rights: {user}")
+    # todo: create pop up that asks for confirmation before adding/removing admin rights or creating the user
     return ""
 
 def makeAllSecurePasswords():
-    return ""
+    flag = False
+    for account in users + admins:
+        command = [
+            "powershell.exe",
+            "-NoProfile",
+            "-Command",
+            f"Set-LocalUser -Name '{account}' -Password (ConvertTo-SecureString -AsPlainText 'Cyb3rP@trIot' -Force)"
+        ]
+        try:
+            subprocess.run(command, check=True)
+        except subprocess.CalledProcessError as e:
+            log.error(f"makeAllSecurePasswords - Failed to reset password for {account}: {e}")
+            flag = True
+
+    return "Failed to reset passwords for some accounts.\n" if flag else ""
 
 def userPasswordsExpire():
-    return ""
+    flag = False
+    for account in users + admins:
+        command = [
+            "powershell.exe",
+            "-NoProfile",
+            "-Command",
+            f"Set-LocalUser -Name '{account}' -PasswordNeverExpires $false"
+        ]
+        try:
+            subprocess.run(command, check=True)
+        except subprocess.CalledProcessError as e:
+            log.error (f"userPasswordsExpire - Failed to set password expiration for {account}: {e}")
+            flag = True
+    return "Failed to set password expiration for some accounts.\n" if flag else ""
 
 def makeUsersChangePasswordNextLogon():
-    return ""
+    flag = False
+    for account in users + admins:
+        command = [
+            "powershell.exe",
+            "-NoProfile",
+            "-Command",
+            f"net user '{account}' /logonpasswordchg:yes"
+        ]
+        try:
+            subprocess.run(command, check=True)
+        except subprocess.CalledProcessError as e:
+            log.error (f"makeUsersChangePasswordNextLogon - Failed to set password change for {account}: {e}")
+            flag = True
+    return "Failed to set password change for some accounts.\n" if flag else ""
 
 def disableDefaultAccounts():
-    return ""
+    flag = False
+    for account in defaults:
+        command = [
+            "powershell.exe",
+            "-NoProfile",
+            "-Command",
+            f"Disable-LocalUser -Name '{account}'"
+        ]
+        try:
+            subprocess.run(command, check=True)
+        except subprocess.CalledProcessError as e:
+            log.error (f"disableDefaultAccounts - Failed to disable account {account}: {e}")
+            flag = True
+    return "Failed to disable some default accounts.\n" if flag else ""
 
 def runAll():
     checks = [
@@ -78,5 +171,6 @@ def runAll():
         (makeAllSecurePasswords, "makeAllSecurePasswords"),
         (userPasswordsExpire, "userPasswordsExpire"),
         (makeUsersChangePasswordNextLogon, "makeUsersChangePasswordNextLogon"),
+        (disableDefaultAccounts, "disableDefaultAccounts"),
     ]
     return "".join(safe_call(func, label) for func, label in checks)
